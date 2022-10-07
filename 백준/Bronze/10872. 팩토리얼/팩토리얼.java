4a37da71c04b454610288ef1147070d8f8782cde
imjinius <103329327+imjinius@users.noreply.github.com>
2022-10-07 23:31:47 +0900

    [Bronze V] Title: 팩토리얼, Time: 76 ms, Memory: 11452 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        System.out.println(factorial(num));
+	}
+	
+	public static int factorial(int num){
+		if(num == 0)
+			return 1;
+		else 
+			return num*factorial(num-1);
+		
+	}
+        
+}
