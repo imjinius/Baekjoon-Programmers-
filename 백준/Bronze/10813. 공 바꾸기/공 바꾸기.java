d65df2d8cda7f3e98f55cc4e9b8e6c42a9193176
imjinius <103329327+imjinius@users.noreply.github.com>
2025-04-17 21:42:25 +0900

    [Bronze II] Title: 공 바꾸기, Time: 68 ms, Memory: 11836 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,36 +1,35 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int a = Integer.parseInt(input[0]);
-        int b = Integer.parseInt(input[1]);
-        int[] basket = IntStream.rangeClosed(1, a).toArray();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i=0;i<b;i++) {
-            String[] change = br.readLine().split(" ");
-            int idx1 = Integer.parseInt(change[0]) - 1;
-            int idx2 = Integer.parseInt(change[1]) - 1;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int[] basket = new int[a];
+        for (int i = 0; i < a; i++) {
+            basket[i] = i + 1;
+        }
+
+        for (int i = 0; i < b; i++) {
+            st = new StringTokenizer(br.readLine());
+            int idx1 = Integer.parseInt(st.nextToken()) - 1;
+            int idx2 = Integer.parseInt(st.nextToken()) - 1;
 
             int temp = basket[idx1];
             basket[idx1] = basket[idx2];
             basket[idx2] = temp;
         }
 
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i<basket.length; i++){
-            sb.append(basket[i]);
-            if (i != basket.length - 1) sb.append(" ");
+        for (int i = 0; i < a; i++) {
+            bw.write(basket[i] + (i == a - 1 ? "\n" : " "));
         }
 
-        System.out.println(sb);
-
+        bw.flush();
+        bw.close();
     }
 }
